d32aff4543c0d40bc8fe745ed9bd1ba3b9c88642
royt81 <107740928+royt81@users.noreply.github.com>
2023-01-16 10:04:30 +0100

    it can do something now

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
 
 
-const root = document.querySelector("#root");
-const input = document.querySelector("#input");
-const submitBtn = document.querySelector("#submit");
-const textLabel = document.querySelector("#textLabel");
+let root = document.querySelector("#root");
+let input = document.querySelector("#input");
+let submitBtn = document.querySelector("#submit");
+let textLabel = document.querySelector("#textLabel");
 
 submitBtn.addEventListener("click", ()=>{
-    let input = input.value
-
-    if(isNaN(input)){
+    let number = input.value
+    console.log(number.value)
+    if(isNaN(number)){
         textLabel.innerText = "You must enter a number!"
     }
     else{
-        creatDice(input)
+        creatDice(number)
     }
 })
 
+function creatDice(input){
+    for(let i=1; i<=input; i++){
+        formDice()
+    }
+}
 
+function formDice(){
+    let diceBox = document.createElement("div");
+    let sidesInput = document.createElement("input");
+    let inputBtn = document.createElement("button")
+    let diceDisplay = document.createElement("h2");
+
+    diceBox.style.width = "200px";
+    diceBox.style.height = "200px";
+    sidesInput.placeholder = "how many sides?";
+    inputBtn.innerText = "SUBMIT";
+    diceDisplay.innerText = 0;
+    
+    diceBox.appendChild(diceDisplay);
+    diceBox.appendChild(inputBtn);
+    diceBox.appendChild(sidesInput);
+    root.appendChild(diceBox);
+    
+    inputBtn.addEventListener("click", ()=>{
+        let input= sidesInput.value;
+        let result = Math.floor(Math.random() * input) + 1;
+        diceDisplay.innerText = result;
+        inputBtn.innerText = "ROLL"
+        }
+    )
+}
 
 
 

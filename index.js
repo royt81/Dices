38ab683048a1def7806fc4465f398397e4f5f8d9
royt81 <107740928+royt81@users.noreply.github.com>
2023-02-16 11:44:25 +0100

    update 16.02

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const textLabel = document.querySelector("#textLabel");
 const resetBtn = document.querySelector("#reset");
 const rollAllBtn = document.querySelector("#rollAll");
 
-
-
 submitBtn.addEventListener("click", ()=>{
 
     root.innerHTML = "";
@@ -15,16 +13,16 @@ submitBtn.addEventListener("click", ()=>{
 
     if(isNaN(number)){
         textLabel.innerText = "You must enter a number!";
-    //    textLabel.style.backgroundColor = "red";
     }
-    if(number>20){
-        textLabel.innerText = "let's keep it to no more then 20, ok?"
-    //    textLabel.style.backgroundColor = "yellow"
+    else if(number>25){
+        textLabel.innerText = "let's keep it to no more then 25, ok?"
     }
-    if(number<=20 && !isNaN(number)){
+    else if(number == 0){
+        textLabel.innerText = "you can do better"
+    }
+    else{
         creatDice(number)
         textLabel.innerText = `making ${number} dices for you!`;
-    //    textLabel.style.backgroundColor = "green";
     }
 })
 
@@ -40,23 +38,35 @@ function formDice(){
     let sidesInput = document.createElement("input");
     let inputBtn = document.createElement("button")
     let diceDisplay = document.createElement("h3");
-
+    let brake = document.createElement("br");
+    
     diceBox.className = "diceBox"
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     diceBox.style.backgroundColor = "#" + randomColor;
-    sidesInput.placeholder = "how many sides?";
+    sidesInput.placeholder = "# of sides?";
+    sidesInput.defaultValue = "6";
+    sidesInput.id = "sideInput";
     inputBtn.innerText = "SUBMIT";
+    inputBtn.id = "diceInputBtn";
     diceDisplay.innerText = 0;
-    
+
     diceBox.appendChild(diceDisplay);
     diceBox.appendChild(inputBtn);
+    diceBox.appendChild(brake);
     diceBox.appendChild(sidesInput);
     root.appendChild(diceBox);
     
     inputBtn.addEventListener("click", ()=>{
+
         let input= sidesInput.value;
         if(isNaN(input)){
             diceDisplay.innerText = "Only numbers please!";
+        }
+        else if(input == 0){
+            diceDisplay.innerText = "More then zero. please!";
+        }
+        else if(input>999999999){
+            diceDisplay.innerText = "we want to stay under a trillion";
         }
         else{
             let result = Math.floor(Math.random() * input) + 1;
@@ -65,21 +75,6 @@ function formDice(){
         }
     })
 }
-
-// function checkInput(){
-//     let input= sidesInput.value;
-        
-//     if(isNaN(input)){
-//         diceDisplay.innerText = "Only numbers please!";
-//     }
-//     else{
-//         let result = Math.floor(Math.random() * input) + 1;
-//         diceDisplay.innerText = result;
-//         inputBtn.innerText = "ROLL"
-//     }
-// }
-
-
 
 resetBtn.addEventListener("click", ()=>{
     root.innerHTML = ""
